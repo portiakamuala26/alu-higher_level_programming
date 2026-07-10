4c3f0bc9e74b3e93ead0e6e706bc30338697f945
portiakamuala26 <p.kamuala@alustudent.com>
2026-07-10 03:44:14 -0700

    Add safe division function

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """Divides two integers and prints the result."""
+    result = None
+
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+
+    return result
