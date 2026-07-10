2112c73fa536e40fe632b258e47d2425653bf2e7
portiakamuala26 <p.kamuala@alustudent.com>
2026-07-10 02:43:02 -0700

    Add function to sum unique integers

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    """Add all unique integers in a list."""
+    unique_numbers = []
+    total = 0
+
+    for number in my_list:
+        if number not in unique_numbers:
+            unique_numbers.append(number)
+            total += number
+
+    return total
