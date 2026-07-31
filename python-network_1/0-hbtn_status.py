febad0ccda633848bde7bb1e67ad22a06e819ae3
portiakamuala26 <p.kamuala@alustudent.com>
2026-07-31 07:57:05 -0700

    Add status script using urllib

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Fetches the status from the ALU intranet
+
+import urllib.request
+
+with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode("utf-8"))
