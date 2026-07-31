4ea30a6367575803143d60ed0e882bacb8dca161
portiakamuala26 <p.kamuala@alustudent.com>
2026-07-31 08:01:33 -0700

    Fix urllib status script documentation

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-# Fetches the status from the ALU intranet
+"""Fetches the status from the ALU intranet."""
 
 import urllib.request
+
 
 with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as response:
     body = response.read()
