b1e7cea102c5380eaa026556e5001d3ce0adf989
portiakamuala26 <p.kamuala@alustudent.com>
2026-07-31 06:17:01 -0700

    make code 3 lines, delete extra space

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL and displays the size of the response body
-
-curl -s -o /dev/null -w "%{size_download}" "$1"
+curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f2
