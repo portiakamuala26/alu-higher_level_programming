e1bb836ef49cf64ad226b235609e88aa3e55aae0
portiakamuala26 <p.kamuala@alustudent.com>
2026-07-31 08:18:36 -0700

    make executable

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""Sends a POST request with an email parameter."""
-
-import sys
-import urllib.parse
-import urllib.request
-
-
-data = urllib.parse.urlencode({"email": sys.argv[2]}).encode("ascii")
-
-with urllib.request.urlopen(sys.argv[1], data=data) as response:
-    print(response.read().decode("utf-8"))
+"""
+Python script to send POST request to given URL with given email as parameter
+"""
+if __name__ == "__main__":
+    from urllib import request, parse
+    from sys import argv
+    if argv[2]:
+        data_1 = {}
+        data_1['email'] = argv[2]
+        data_2 = parse.urlencode(data_1)
+        data = data_2.encode("UTF-8")
+    if argv[1]:
+        req = request.Request(argv[1], data)
+        with request.urlopen(req) as response:
+            content = response.read()
+            print(content.decode("UTF-8"))
