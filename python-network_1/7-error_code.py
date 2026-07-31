3e5efde7511a0277143452e4a62a4cf5ffdb0046
portiakamuala26 <p.kamuala@alustudent.com>
2026-07-31 08:36:29 -0700

    Handle HTTP errors using requests

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script to send request to given URL
+and display body of response, decodes, error in HTTP status code is >= 400
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    response = requests.get(argv[1])
+    status = response.status_code
+    if status >= 400:
+        print("Error code: {}".format(status))
+    else:
+        print(response.text)
