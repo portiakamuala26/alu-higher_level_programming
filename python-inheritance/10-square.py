263ab32391e7ea6454d58ba994e17d0bef0e7a6c
portiakamuala26 <p.kamuala@alustudent.com>
2026-07-31 00:33:32 -0700

    Add Square class

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that defines Square."""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Representation of a square."""
+
+    def __init__(self, size):
+        """Initialize a square with size."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
