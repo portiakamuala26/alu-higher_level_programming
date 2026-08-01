c785f4b984960b07ed1e8ea58674daef17aa36c8
portiakamuala26 <p.kamuala@alustudent.com>
2026-08-01 00:40:29 -0700

    Prevent SQL injection in state filter

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Lists states matching user input safely from hbtn_0e_0_usa."""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    query = (
+        "SELECT * FROM states WHERE BINARY name = %s "
+        "ORDER BY id ASC"
+    )
+
+    cursor.execute(query, (sys.argv[4],))
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
