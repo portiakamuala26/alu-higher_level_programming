91afb13728f4459dff91716d4a9ea6e776babc73
portiakamuala26 <p.kamuala@alustudent.com>
2026-08-01 00:34:26 -0700

    Fix pycodestyle formatting

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -17,8 +17,9 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(
-        sys.argv[4]
+    query = (
+        "SELECT * FROM states WHERE BINARY name = '{}' "
+        "ORDER BY id ASC".format(sys.argv[4])
     )
 
     cursor.execute(query)
