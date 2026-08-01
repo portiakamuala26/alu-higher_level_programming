f824fe006067ede4577d0b64c1c253d476bb15b1
portiakamuala26 <p.kamuala@alustudent.com>
2026-08-01 00:32:24 -0700

    Filter states by user input with case sensitivity

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(
         sys.argv[4]
     )
 
