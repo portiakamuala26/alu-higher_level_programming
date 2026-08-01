0612f9e49ea7b5aa53e0b1f297bf419ab74612bc
portiakamuala26 <p.kamuala@alustudent.com>
2026-07-31 22:37:24 -0700

    Add Rectangle area method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,12 +8,17 @@ class Rectangle(Base):
     """Represents a rectangle."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes a Rectangle."""
         super().__init__(id)
 
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+
+    def area(self):
+        """Returns the area of the rectangle."""
+        return self.__width * self.__height
 
     @property
     def width(self):
