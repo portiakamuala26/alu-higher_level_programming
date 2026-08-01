61f049f2c37f62db5ae84a0490672b6559d76de1
portiakamuala26 <p.kamuala@alustudent.com>
2026-07-31 23:23:12 -0700

    Add JSON string conversion method to Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
 """Defines the Base class."""
 
+import json
+
 
 class Base:
-    """Base class for future project classes."""
+    """Represents the base class for all models."""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a Base object."""
+        """Initializes a Base instance."""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of a list of dictionaries."""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+
+        return json.dumps(list_dictionaries)
